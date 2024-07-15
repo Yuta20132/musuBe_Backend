515279692e08a39ea35cfe385f8c4231c8c7bf0f
yutaxxx <yuta2013docomo@gmail.com>
2024-07-15 22:03:09 +0900

    大幅な変更

---
+++ myproject/account/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import serializers
+from .models import User
+import re
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name', 'affiliation')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_email(self, value):
+        affiliation = self.initial_data.get('affiliation')
+        if '大学' in affiliation or '研究所' in affiliation:
+            if not value.endswith('ac.jp'):
+                raise serializers.ValidationError("大学や研究所の所属の場合、メールアドレスはac.jpドメインである必要があります。")
+        return value
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            affiliation=validated_data['affiliation'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for musuBe_Backend project.
+WSGI config for myproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "musuBe_Backend.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
 application = get_wsgi_application()
